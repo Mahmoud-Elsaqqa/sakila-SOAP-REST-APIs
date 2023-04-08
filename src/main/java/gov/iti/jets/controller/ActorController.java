6c39279cc634116b5aadc400da6f85b4bac38b10
Mahmoud-Elsaqqa <elsaqqa.mou@gmail.com>
2023-04-09 00:52:31 +0200

    Adding Interface CrudController providing a crud-endpoints template for all controllers

--- src/main/java/gov/iti/jets/controller/ActorController.java
+++ src/main/java/gov/iti/jets/controller/ActorController.java
@@ -1,36 +1,49 @@
 package gov.iti.jets.controller;
 
 import gov.iti.jets.model.dto.core.ActorModel;
-import gov.iti.jets.model.entity.ActorEntity;
-import gov.iti.jets.model.mapping.mapper.ActorMapper;
 import gov.iti.jets.service.ActorService;
 import gov.iti.jets.service.ActorServiceImpl;
-import gov.iti.jets.util.EntityManagerHelper;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.TypedQuery;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
+import java.util.List;
 import java.util.Optional;
 
 
 @Path("/actors")
-public class ActorController {
+public class ActorController implements CrudController<ActorModel> {
 
-        ActorService actorService = new ActorServiceImpl();
+    ActorService actorService = new ActorServiceImpl();
 
-    @Path("/{id}")
+    @Override
     @GET
+    @Path("/{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public ActorModel getActor(@PathParam("id") String id) {
-
-        /*
-        Testing service and dataAccess Layers
-        */
+    public ActorModel getById(@PathParam("id") String id) {
         Optional<ActorModel> actor = actorService.getActor(id);
         return actor.get();
+    }
+
+    @Override
+    public List<ActorModel> list() {
+        return null;
+    }
+
+    @Override
+    public ActorModel add(ActorModel resource) {
+        return null;
+    }
+
+    @Override
+    public ActorModel update(String id, ActorModel resource) {
+        return null;
+    }
+
+    @Override
+    public void delete(String id) {
+
     }
 }
