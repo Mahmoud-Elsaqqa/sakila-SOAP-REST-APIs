38c44b3f8088a361b19d8b175b581b251293dbae
Mahmoud-Elsaqqa <elsaqqa.mou@gmail.com>
2023-04-09 00:16:38 +0200

    Testing Actor DataAccess and Service Layer

--- src/main/java/gov/iti/jets/controller/ActorController.java
+++ src/main/java/gov/iti/jets/controller/ActorController.java
@@ -3,6 +3,8 @@ package gov.iti.jets.controller;
 import gov.iti.jets.model.dto.core.ActorModel;
 import gov.iti.jets.model.entity.ActorEntity;
 import gov.iti.jets.model.mapping.mapper.ActorMapper;
+import gov.iti.jets.service.ActorService;
+import gov.iti.jets.service.ActorServiceImpl;
 import gov.iti.jets.util.EntityManagerHelper;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.TypedQuery;
@@ -12,19 +14,23 @@ import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
+import java.util.Optional;
+
 
 @Path("/actors")
 public class ActorController {
 
+        ActorService actorService = new ActorServiceImpl();
 
     @Path("/{id}")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public ActorModel getActor(@PathParam("id") Integer id) {
-        EntityManager entityManager = EntityManagerHelper.getEntityManager();
-        TypedQuery<ActorEntity> query = entityManager.createQuery("from actor where actorId=:id", ActorEntity.class);
-        query.setParameter("id", id);
-        ActorEntity entity = query.getSingleResult();
-        return ActorMapper.INSTANCE.mapToDto(entity);
+    public ActorModel getActor(@PathParam("id") String id) {
+
+        /*
+        Testing service and dataAccess Layers
+        */
+        Optional<ActorModel> actor = actorService.getActor(id);
+        return actor.get();
     }
 }
