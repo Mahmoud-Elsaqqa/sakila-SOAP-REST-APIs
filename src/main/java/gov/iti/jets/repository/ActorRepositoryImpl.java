b0556f5336893ef1e96b30724a95a54e9d7e3197
Mahmoud-Elsaqqa <elsaqqa.mou@gmail.com>
2023-04-11 00:58:42 +0200

    Major Repository Refactoring: More CRUD Abstraction, providing a single generic implementation of the basic CRUDs of the CrudRepository Interface, eliminating boilerplate code of queries/transactions. Each EntityRepository just extends the CrudRepositoryImpl class and call the super constructor with it's class type.

--- src/main/java/gov/iti/jets/repository/ActorRepositoryImpl.java
+++ src/main/java/gov/iti/jets/repository/ActorRepositoryImpl.java
@@ -13,109 +13,45 @@ import jakarta.persistence.criteria.Root;
 import java.util.List;
 import java.util.Optional;
 
-public class ActorRepositoryImpl implements CrudRepository<ActorEntity, Integer> {
+public class ActorRepositoryImpl implements CrudRepository<ActorEntity> {
 
     @Override
     public List<ActorEntity> findAll() {
-        EntityManager entityManager = EntityManagerHelper.getEntityManager();
-        TypedQuery<ActorEntity> typedQuery = entityManager
-                .createQuery("from actor", ActorEntity.class);
-        return typedQuery.getResultList();
+        return null;
     }
 
     @Override
     public void deleteById(Integer id) {
-        try (EntityManager entityManager = EntityManagerHelper.getEntityManager()) {
-            CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
-            CriteriaDelete<ActorEntity> delete = criteriaBuilder.createCriteriaDelete(ActorEntity.class);
-            Root<ActorEntity> actorEntityRoot = delete.from(ActorEntity.class);
-            delete.where(criteriaBuilder.equal(actorEntityRoot.get("actorId"), id));
-            entityManager.createQuery(delete).executeUpdate();
-        } catch (Exception e) {
-            EntityManagerHelper.rollback();
-            throw new PersistenceException("Couldn't delete Actor");
-        }
+
     }
 
     @Override
     public void delete(ActorEntity entity) {
-        try (EntityManager entityManager = EntityManagerHelper.getEntityManager()) {
-            CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
-            CriteriaDelete<ActorEntity> delete = criteriaBuilder.createCriteriaDelete(ActorEntity.class);
-            Root<ActorEntity> actorEntityRoot = delete.from(ActorEntity.class);
-            delete(entity);
-            entityManager.createQuery(delete).executeUpdate();
-        } catch (Exception e) {
-            EntityManagerHelper.rollback();
-            throw new PersistenceException("Couldn't delete Actor");
-        }
+
     }
 
     @Override
     public boolean existsById(Integer id) {
-        EntityManager entityManager = EntityManagerHelper.getEntityManager();
-        TypedQuery<Long> typedQuery = entityManager
-                .createQuery("SELECT count(a) FROM actor a WHERE actorId=:id", Long.class);
-        typedQuery.setParameter("id", id);
-        return typedQuery.getSingleResult() > 0;
+        return false;
     }
 
     @Override
     public long count() {
-        EntityManager entityManager = EntityManagerHelper.getEntityManager();
-        TypedQuery<Long> typedQuery = entityManager
-                .createQuery("SELECT count(a) FROM actor a", Long.class);
-        return typedQuery.getSingleResult();
+        return 0;
     }
 
     @Override
     public Optional<ActorEntity> findById(Integer id) {
-        EntityManager entityManager = EntityManagerHelper.getEntityManager();
-        TypedQuery<ActorEntity> typedQuery = entityManager
-                .createQuery("from actor where actorId=:id", ActorEntity.class);
-        typedQuery.setParameter("id", id);
-        ActorEntity entity = typedQuery.getSingleResult();
-        return Optional.ofNullable(entity);
+        return Optional.empty();
     }
 
     @Override
     public void save(ActorEntity entity) {
-        try (EntityManager entityManager = EntityManagerHelper.getEntityManager()) {
-            EntityManagerHelper.beginTransaction();
-            entityManager.persist(entity);
-            EntityManagerHelper.commit();
-        } catch (Exception e) {
-            EntityManagerHelper.rollback();
-        }
+
     }
 
     @Override
     public void update(ActorEntity entity) {
-        try (EntityManager entityManager = EntityManagerHelper.getEntityManager()) {
-            CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
-            CriteriaUpdate<ActorEntity> update = criteriaBuilder.createCriteriaUpdate(ActorEntity.class);
-            Root<ActorEntity> actorEntityRoot = update.from(ActorEntity.class);
-            update(entity);
-            entityManager.createQuery(update).executeUpdate();
-        } catch (Exception e) {
-            EntityManagerHelper.rollback();
-            throw new PersistenceException("Couldn't delete Actor");
-        }
-
-    }
-
-    @Override
-    public void updateById(Integer id) {
-        try (EntityManager entityManager = EntityManagerHelper.getEntityManager()) {
-            CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
-            CriteriaUpdate<ActorEntity> update = criteriaBuilder.createCriteriaUpdate(ActorEntity.class);
-            Root<ActorEntity> actorEntityRoot = update.from(ActorEntity.class);
-            update.where(criteriaBuilder.equal(actorEntityRoot.get("actorId"), id));
-            entityManager.createQuery(update).executeUpdate();
-        } catch (Exception e) {
-            EntityManagerHelper.rollback();
-            throw new PersistenceException("Couldn't delete Actor");
-        }
 
     }
 }
--- src/main/java/gov/iti/jets/repository/CrudRepository.java
+++ src/main/java/gov/iti/jets/repository/CrudRepository.java
@@ -1,28 +1,28 @@
 package gov.iti.jets.repository;
 
 import gov.iti.jets.model.entity.BaseEntity;
+
 import java.util.List;
 import java.util.Optional;
+
 /**
  * Inspired By CrudRepository of Spring-Data framework
- * */
-public interface CrudRepository<T extends BaseEntity, ID> {
+ */
+public interface CrudRepository<T extends BaseEntity> {
 
     List<T> findAll();
 
-    void deleteById(ID id);
+    void deleteById(Integer id);
 
     void delete(T entity);
 
-    boolean existsById(ID id);
+    boolean existsById(Integer id);
 
     long count();
 
-    Optional<T> findById(ID id);
+    Optional<T> findById(Integer id);
 
     void save(T entity);
 
     void update(T entity);
-
-    void updateById(ID id);
 }
